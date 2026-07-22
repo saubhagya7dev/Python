ea9036a43862fc686ba68d7e3dbf6d6e84865852
saubhagya <saubhagya7developer@gmail.com>
2026-07-22 14:40:48 +0530

    Square root of given no.

--- yashi.py
+++ yashi.py
@@ -1,5 +1,12 @@
-# Calculate area of rectangle
+import math
 
+# Add two no.
+num1 = float(input("enter the first no."))
+num2 = float(input("enter 2nd no."))
+sum = num1 + num2
+print("the sum of given no. is: ", sum)
+
+# Calculate area of rectangle
 length = int(input("Enter the length of rectangle: "))
 breadth = int(input("Enter the breadth of rectangle: "))
 area = length * breadth
@@ -15,8 +22,7 @@ amount = principle + simple_interest
 print("Simple Interest:", simple_interest)
 print("Total Amount:", amount)
 
-# Add two no.
-num1 = float(input("enter the first no."))
-num2 = float(input("enter 2nd no."))
-sum = num1 + num2
-print("the sum of given no. is: ", sum)
+# Square root of the given no.
+num = int(input("Enter the no. to calculate the square root"))
+sr = math.sqrt(num)
+print("square root of the given no. is: ", sr)
