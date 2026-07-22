b61f9d27de1a69c9d9c9459a4fb332a9e2195407
saubhagya <saubhagya7developer@gmail.com>
2026-07-22 14:33:12 +0530

    Add two no.

--- yashi.py
+++ yashi.py
@@ -14,3 +14,9 @@ simple_interest = (principle * rate * time) / 100
 amount = principle + simple_interest
 print("Simple Interest:", simple_interest)
 print("Total Amount:", amount)
+
+# Add two no.
+num1 = float(input("enter the first no."))
+num2 = float(input("enter 2nd no."))
+sum = num1 + num2
+print("the sum of given no. is: ", sum)
